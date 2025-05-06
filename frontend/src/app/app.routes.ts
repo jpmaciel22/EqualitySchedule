9e51b6f380ce5088d5d98048189a08cf98332cc1
jpmaciel22 <jpbmaciel@gmail.com>
2025-05-06 19:17:46 -0300

    Not found pronto!

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { authGuard } from './guards/auth.guard';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent},
-    { path: '', component: HomeComponent, canActivate: [authGuard]}
+    { path: '', component: HomeComponent, canActivate: [authGuard]},
+    {path: '**', component: NotFoundComponent},
 ];
